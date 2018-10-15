f64deda0275338e87a9fbdf82eed631cca4ba569
Micah Jones <maj407@gmail.com>
2018-10-15 14:23:05 -0400

    adding flash functionality

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
 new Vue ({
 
-  el: '#gameboard'
-
-  
+  el: '#gameboard',
 
 
+  data: {
+    blueFlash: false,
+    greenFlash: false,
+    yellowFlash: false,
+    redFlash: false,
+    userSequence: [],
+    ComputerSequence: []
+
+  },
+
+  methods: {
+    start: function () {
+      flash()
+
+    },
+
+    button: function (color) {
+      this.flash(color);
+
+    },
+
+    flash: function (color) {
+      if (color == 'green'){
+        this.greenFlash = true;
+      }else if (color == 'red'){
+        this.redFlash = true;
+      }else if (color == 'blue'){
+        this.blueFlash = true;
+      }else if (color == 'yellow'){
+        this.yellowFlash = true;
+      }
+
+    },
+
+    play: function () {
+
+    //  computerSequence.push
+    }
+
+}
 })
