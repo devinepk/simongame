3d33c2b7ef140f262a5936ca80daaaa1bb23f80c
Ben Patterson <bmizepatterson@gmail.com>
2018-10-15 15:02:09 -0400

    Adds random color selector and logic to button() function

--- app.js
+++ app.js
@@ -9,18 +9,44 @@ new Vue ({
     yellowFlash: false,
     redFlash: false,
     userSequence: [],
-    ComputerSequence: []
+    computerSequence: [],
+    currentIndex = 0
 
   },
 
   methods: {
     start: function () {
-      flash()
+
+      let randomColor = this.getRandomColor();
+      this.computerSequence.push(randomColor);
+      this.flash(this.computerSequence[0]);
 
     },
 
     button: function (color) {
+
       this.flash(color);
+      // What index are we at?
+
+      // check that this was the right color
+      if (color == this.computerSequence[this.currentIndex]) {
+
+        if (this.currentIndex == this.computerSequence.length - 1) {
+
+          this.computerPlay();
+
+        } else {
+
+          this.currentIndex++;
+
+        }
+
+      } else {
+
+        // Game is over
+
+      }
+
 
     },
 
@@ -35,12 +61,41 @@ new Vue ({
         this.yellowFlash = true;
       }
 
+      let vue = this;
+      setTimeout(function() {
+        vue.greenFlash = false;
+        vue.redFlash = false;
+        vue.blueFlash = false;
+        vue.yellowFlash = false;
+      }, 500);
+
     },
 
     play: function () {
+      // flash ( color1)
+      // setTimeout(flash (color2)  , 500)
+
+
 
     //  computerSequence.push
+    },
+
+    getRandomColor: function() {
+      let random = Math.ceil(Math.random() * 4);
+
+      if (random == 1) {
+        return 'red';
+      }
+      if (random == 2) {
+        return 'blue';
+      }
+      if (random == 3) {
+        return 'green';
+      }
+      if (random == 4) {
+        return 'yellow';
+      }
     }
 
-}
+  }
 })
